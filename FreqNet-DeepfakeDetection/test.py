15e2c3da23c976421c8dbcccce04641ac23cfbff
Dai <daiwb2000@outlook.com>
2025-05-04 17:07:24 +1000

    Debug

--- FreqNet-DeepfakeDetection/test.py
+++ FreqNet-DeepfakeDetection/test.py
@@ -8,55 +8,66 @@ from validate import validate
 from networks.freqnet import freqnet
 from options.test_options import TestOptions
 import numpy as np
-import random
-
 
 DetectionTests = {
-                'ForenSynths': { 'dataroot'   : '/opt/data/private/DeepfakeDetection/ForenSynths/',
-                                 'no_resize'  : False, # Due to the different shapes of images in the dataset, resizing is required during batch detection.
-                                 'no_crop'    : True,
-                               },
-
-           'GANGen-Detection': { 'dataroot'   : '/opt/data/private/DeepfakeDetection/GANGen-Detection/',
-                                 'no_resize'  : True,
-                                 'no_crop'    : True,
-                               },
-
-                 }
-
+    'Celeb-DF': {
+        'dataroot': './dataset/test/Celeb-DF',
+        'no_resize': False,  # Due to the different shapes of images in the dataset, resizing is required during batch detection.
+        'no_crop': True,
+    },
+    'DFD': {
+        'dataroot': './dataset/test/DFD',
+        'no_resize': False,  # Due to the different shapes of images in the dataset, resizing is required during batch detection.
+        'no_crop': True,
+    },
+    'DFDC': {
+        'dataroot': './dataset/test/DFDC',
+        'no_resize': False,  # Due to the different shapes of images in the dataset, resizing is required during batch detection.
+        'no_crop': True,
+    },
+    'DFDCP': {
+        'dataroot': './dataset/test/DFDCP',
+        'no_resize': False,  # Due to the different shapes of images in the dataset, resizing is required during batch detection.
+        'no_crop': True,
+    },
+    'FF++': {
+        'dataroot': './dataset/test/FF++',
+        'no_resize': False,  # Due to the different shapes of images in the dataset, resizing is required during batch detection.
+        'no_crop': True,
+    },
+    'UADFA': {
+        'dataroot': './dataset/test/UADFA',
+        'no_resize': False,  # Due to the different shapes of images in the dataset, resizing is required during batch detection.
+        'no_crop': True,
+    }
+}
 
 opt = TestOptions().parse(print_options=False)
 print(f'Model_path {opt.model_path}')
 
-# get model
+# Load model
 model = freqnet(num_classes=1)
-
-# from collections import OrderedDict
-# from copy import deepcopy
-# state_dict = torch.load(opt.model_path, map_location='cpu')['model']
-# pretrained_dict = OrderedDict()
-# for ki in state_dict.keys():
-    # pretrained_dict[ki[7:]] = deepcopy(state_dict[ki])
-# model.load_state_dict(pretrained_dict, strict=True)
-
 model.load_state_dict(torch.load(opt.model_path, map_location='cpu'), strict=True)
 model.cuda()
 model.eval()
 
-
+# Iterate over test datasets
 for testSet in DetectionTests.keys():
     dataroot = DetectionTests[testSet]['dataroot']
     printSet(testSet)
 
-    accs = [];aps = []
-    print(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()))
-    for v_id, val in enumerate(os.listdir(dataroot)):
-        opt.dataroot = '{}/{}'.format(dataroot, val)
-        opt.classes  = '' #os.listdir(opt.dataroot) if multiclass[v_id] else ['']
-        opt.no_resize = DetectionTests[testSet]['no_resize']
-        opt.no_crop   = DetectionTests[testSet]['no_crop']
-        acc, ap, _, _, _, _ = validate(model, opt)
-        accs.append(acc);aps.append(ap)
-        print("({} {:12}) acc: {:.1f}; ap: {:.1f}".format(v_id, val, acc*100, ap*100))
-    print("({} {:10}) acc: {:.1f}; ap: {:.1f}".format(v_id+1,'Mean', np.array(accs).mean()*100, np.array(aps).mean()*100));print('*'*25) 
+    # Update dataroot in options
+    opt.dataroot = dataroot
 
+    # Validate and get metrics
+    acc, ap, auc, f1, r_acc, f_acc, y_true, y_pred = validate(model, opt)
+
+    # Print metrics for the current test set
+    print(f"Results for {testSet}:")
+    print(f"Accuracy: {acc:.4f}")
+    print(f"Average Precision: {ap:.4f}")
+    print(f"AUC: {auc:.4f}")
+    print(f"F1-Score: {f1:.4f}")
+    print(f"Accuracy of Real Images (RACC): {r_acc:.4f}")
+    print(f"Accuracy of Fake Images (FACC): {f_acc:.4f}")
+    print('*' * 50)
